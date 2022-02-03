5d7aea298c80566d4efad4453da841cccfbd0acb
Andres Melendes <andresruse18@gmail.com>
2022-02-03 09:11:24 -0400

    Pantry v 1.0

--- data_base.py
+++ data_base.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import os
 
-engine = create_engine(os.environ.get('DATABASE_URL', 'sqlite:///DataBase.db'))
+engine = create_engine('sqlite:///DataBase.db')
 db = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
